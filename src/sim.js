15feb06d63aac4b75b317fa28652b3895de772f6
Arthur Portas <arthurportas@gmail.com>
2025-12-22 22:16:25 +0000

    Add Nitrox 28 and Nitrox 32 to Bottom Gas dropdown, remove '(Trimix)' from label

--- src/sim.js
+++ src/sim.js
@@ -61,6 +61,10 @@ export function computeDecompressionSchedule({ depth, time, gasLabel, gfLow, gfH
     ? { o2:0.18, he:0.45 }
     : gasLabel === '21/35'
     ? { o2:0.21, he:0.35 }
+    : gasLabel === '28'
+    ? { o2:0.28, he:0 }
+    : gasLabel === '32'
+    ? { o2:0.32, he:0 }
     : gasLabel === 'o2'
     ? { o2:1, he:0 }
     : { o2:0.21, he:0 };
